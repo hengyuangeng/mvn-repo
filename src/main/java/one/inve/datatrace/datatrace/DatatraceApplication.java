6ac6d86fed6d8318684f032a3e0b3881c6cc36f0
hengyuangeng <41353913+hengyuangeng@users.noreply.github.com>
2019-05-20 16:11:29 +0800

    Add files via upload

---
+++ src/main/java/one/inve/datatrace/datatrace/DatatraceApplication.java
@@ -0,0 +1,13 @@
+package one.inve.datatrace.datatrace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatatraceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DatatraceApplication.class, args);
+    }
+
+}
